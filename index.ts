e3b4d44c342ff6aaf6eac4f357aacb5087a79242
Alexander Makarevich <alexander.makarevich@eltex.loc>
2022-09-29 17:33:44 +0700

    coupling var

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
-import { Client } from './coupling-1/client';
+// import { Client } from './coupling-1/client';
+import { Client } from './coupling-2/client';
 
 const client = new Client();
 client.work();
