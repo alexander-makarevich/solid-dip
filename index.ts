2b7d0bf78a2d73f6d4b7f8f2ed0c4738816a1200
Alexander Makarevich <alexander.makarevich@eltex.loc>
2022-09-30 10:18:51 +0700

    not-coupling-3 added

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
 // import { Client } from './coupling-1/client';
 // import { Client } from './coupling-2/client';
 // import { Client } from './not-coupling-1/client';
-import { Client } from './not-coupling-2/client';
+// import { Client } from './not-coupling-2/client';
+import { Client } from './not-coupling-3/client';
 
 const client = new Client();
 client.work();
